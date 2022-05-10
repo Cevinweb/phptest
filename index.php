e01097ad73e495923daa7dcb695bc081b21c90f0
Cevinweb <Cevinweb@gmail.com>
2022-05-10 16:49:41 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$arr = [
+	[
+		'Title' => 'One',
+		'Price' => '100',
+		'Discription' => 'Discription'
+	],
+	[
+		'Title' => 'Tool',
+		'Price' => '500',
+		'Discription' => 'Discription'
+	],
+	[
+		'Title' => 'sigarets',
+		'Price' => '50',
+		'Discription' => 'Discription'
+	]
+];
+
+echo "<pre>";
+print_r($arr);
+echo "</pre>";
