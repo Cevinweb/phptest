e2b513510fa801fa25ddab4239e9a1fa627c0333
Cevinweb <Cevinweb@gmail.com>
2022-05-18 20:51:26 +0300

    Удалил все.

--- index.php
+++ index.php
@@ -1,25 +1,2 @@
 <?php
-//тест комментарий
 
-$arr = [
-	[
-		'Title' => 'One',
-		'Price' => '100',
-		'Discription' => 'Discription'
-	],
-	[
-		'Title' => 'Tool',
-		'Price' => '500',
-		'Discription' => 'Discription'
-	],
-	[
-		'Title' => 'sigarets',
-		'Price' => '50',
-		'Discription' => 'Discription'
-	]
-];
-// новый комментарий
-echo "<pre>";
-print_r($arr);
-echo "</pre>";
-// новый комментарий
